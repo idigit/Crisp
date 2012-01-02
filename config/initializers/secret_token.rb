824f58ff15666cc955467c9d56d961309a333649
David Myers <david@dailybooth.com>
2012-01-01 19:03:34 -0500

    added rails stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crisp::Application.config.secret_token = '74d805b3c9fa60fe9b087195204a2216fc9881a6009a9601c4fd476493572754128cffaed3403825689264d285431b230c3444a805d00b8fb5ad7c6f6e0dab0f'
